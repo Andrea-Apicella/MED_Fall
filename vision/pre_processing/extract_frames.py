5202bc99297d0cbea3aa45ae60d64d9a28926dce
Andrea-Apicella <andrea.apicella221@gmail.com>
2022-08-14 10:24:26 +0000

    changed FramesExtractor file name, added copy of extracted frames to persitent

--- vision/pre_processing/extract_frames.py
+++ vision/pre_processing/extract_frames.py
@@ -4,8 +4,10 @@ import cv2
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import os
+import shutil
 from tqdm.auto import tqdm, trange
-from utils.utility_functions import listdir_nohidden_sorted
+from utils.utility_functions import listdir_nohidden_sorted, safe_mkdir
 
 
 class FramesExtractor:
@@ -23,9 +25,9 @@ class FramesExtractor:
         extracted_frames = []
 
         ground_truth = pd.DataFrame()
-        for _, folder in enumerate(self.videos_paths[:self.available_sequences]):
+        for _, folder in enumerate(t0 := tqdm(self.videos_paths[:self.available_sequences])):
             folder_name = folder.replace(self.videos_folder, "")[1:]
-            # t0.set_description(f"Processing folder: {folder_name}")
+            t0.set_description(f"Processing folder: {folder_name}")
 
             sheet_name = folder.replace(self.videos_folder, "").lower()[1:]
             try:
@@ -46,36 +48,42 @@ class FramesExtractor:
 
             videos = listdir_nohidden_sorted(videos_path)
             folder_frames = []
-            for _, cam in enumerate((t1 := tqdm(videos))):
+            for _, cam in enumerate((t1 := tqdm(videos, leave=False))):
                 start = cam.rfind("/") + 1
                 end = len(cam) - 4
                 t1.set_description(f'Extracting frames from: {cam[start:].replace(" ", "_")}')
                 cap = cv2.VideoCapture(cam)
                 try:
                     n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-                    for f in trange(n_frames):
+                    for f in trange(n_frames, leave=False):
                         ret, frame = cap.read()
                         if not ret:
                             break
                         frame = tf.keras.preprocessing.image.smart_resize(frame, self.frame_size)
-                        # persistent/cam
-                        # actor_1_bed_cam_1_0000
                         frame_name = f"{cam[start:end].replace(' ', '_')}_{str(f).zfill(4)}.jpg"
                         frame_name = frame_name.lower()
                         frame_path = f"{self.output_folder}/{frame_name}"
                     
-                        #print(frame_path)
                         cv2.imwrite(frame_path, frame)
-                        # extracted_frames.append(frame)
-                        # features = self.predict_frame(frame)
-                        # folder_features.append(features)
-                        #
-                        # file_name = f'{cam[start:end].lower().replace(" ", "_")}_{str(f).zfill(4)}'
-                        # frames_names.append(file_name)
 
                 finally:
                     cap.release()
+                    
 
-            ground_truth = pd.concat([ground_truth, folder_ground_truth], axis=0, ignore_index=True)
-            ground_truth.drop(columns=ground_truth.columns[0], axis=1, inplace=True)
-        return ground_truth
+if __name__ == "__main__":
+    
+    projectdir = "/home/jovyan/work/MED_Fall/"
+    
+    fe = FramesExtractor(
+        videos_folder       = f"{projectdir}/vision/vision_dataset/DATASET_WP8_resized/",
+        output_folder       = f"{projectdir}/vision/vision_dataset/extracted_frames/",
+        labels              = f"{projectdir}/dataset/labels.xlsx",
+        ground_truth_folder = f"{projectdir}/vision/vision_dataset/ground_truth/",
+    )
+    
+    fe.extract_frames()
+    
+    extracted_frames_persistent = '/home/jovyan/work/persistent/extracted_frames/'
+    dir_created = safe_mkdir(extracted_frames_persitent)
+    if dir_created:
+        shutil.copy(fe.output_folder, extracted_frames_persistent)
