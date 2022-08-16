ef182c62957568a34baff466b065243691dd0147
Andrea <andrea.apicella221@gmail.com>
2022-08-16 17:58:38 +0200

    refactored TesseractOCR code. Working

--- dataset/scripts/tesseract_ocr.py
+++ dataset/scripts/tesseract_ocr.py
@@ -4,85 +4,87 @@ import cv2
 import pytesseract
 import numpy as np
 import re
+from tqdm.auto import trange
 
 
-def total_frames(video_path):
-    cap = cv2.VideoCapture(video_path)
-    _, _ = cap.read()
-    n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    print("Total number of frames in the video:", n_frames)
-    cap.release()
-    return n_frames
+class TesseractOCR:
+    def __init__(self, video_path: str):
+        self.video_path = video_path
 
+    def __total_frames(self):
+        cap = cv2.VideoCapture(self.video_path)
+        _, _ = cap.read()
+        n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print("Total number of frames in the video:", n_frames)
+        cap.release()
+        return n_frames
 
-def find_rois(frame) -> tuple[np.array, np.array]:
-    img = frame[:, 0:960]
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    thresh_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    contours, _ = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    def __enhance_text(self, image: np.array) -> np.array:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
+        thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        return thresh
 
-    d = {}
+    def __strip_unwanted_chars_validate_format(self, text: str, n: int):
 
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        bounding_rect = cv2.boundingRect(cnt)
-        d[area] = bounding_rect
+        text = re.sub('\D', '', text)
+        assert len(text) == 9, print(f"Faulty extraction at frame {n}. Text extracted: {text}")
 
-    d_sorted = sorted(d, reverse=True)
-    areas = d_sorted[1:3]
+        text = f"{text[0:2]}:{text[2:4]}:{text[4:6]}.{text[6:]}"
+        return text
 
-    timestamp_roi = d[areas[0]]
-    datalogger_roi = d[areas[1]]
+    def find_rois(self, frame: np.array) -> tuple[np.array, np.array]:
 
-    top_x, top_y, bottom_x, bottom_y = timestamp_roi
-    timestamp_image_region = frame[top_y: top_y + bottom_y, top_x: top_x + bottom_x]
-    top_x, top_y, bottom_x, bottom_y = datalogger_roi
-    datalogger_image_region = frame[top_y: top_y + bottom_y, top_x: top_x + bottom_x]
+        h, w = frame.shape[:-1]
+        img = frame[:, 0:int(w / 2)]
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        thresh_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        contours, _ = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+        d = {}
 
-    return timestamp_image_region, datalogger_image_region
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            bounding_rect = cv2.boundingRect(cnt)
+            d[area] = bounding_rect
 
+        d_sorted = sorted(d, reverse=True)
+        areas = d_sorted[1:3]
 
-def enhance_text(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
-    thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    return thresh
+        timestamp_roi = d[areas[0]]
+        datalogger_roi = d[areas[1]]
 
-video_path = '/Users/andrea/Documents/Github/MED_Fall/dataset/data/Actor_1_Bed_Rolling CAM 8.mp4'
+        top_x, top_y, bottom_x, bottom_y = timestamp_roi
+        timestamp_image_region = frame[top_y: top_y + bottom_y, top_x: top_x + bottom_x]
+        top_x, top_y, bottom_x, bottom_y = datalogger_roi
+        datalogger_image_region = frame[top_y: top_y + bottom_y, top_x: top_x + bottom_x]
 
-cap = cv2.VideoCapture(video_path)
+        return timestamp_image_region, datalogger_image_region
 
-n_frames = total_frames(video_path)
+    def extract_timestamp_datalogger(self) -> tuple[list, list]:
 
-l = []
+        n_frames = self.__total_frames()
+        cap = cv2.VideoCapture(self.video_path)
 
-filter = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-for n in range(n_frames):
-    ret, frame = cap.read()
-    timestamp_image_region, datalogger_image_region = find_rois(frame)
+        timestamps = [None] * n_frames
+        dataloggers = [None] * n_frames
 
-    timestamp_img = enhance_text(timestamp_image_region)
-    datalogger_img = enhance_text(datalogger_image_region)
+        for n in (t := trange(n_frames)):
+            ret, frame = cap.read()
+            timestamp_image_region, datalogger_image_region = self.find_rois(frame)
 
+            timestamp_img = self.__enhance_text(timestamp_image_region)
+            datalogger_img = self.__enhance_text(datalogger_image_region)
 
-    timestamp_text = pytesseract.image_to_string(timestamp_img)
-    datalogger_text = pytesseract.image_to_string(datalogger_img)
+            timestamp_text = pytesseract.image_to_string(timestamp_img)
+            datalogger_text = pytesseract.image_to_string(datalogger_img)
 
-    timestamp_text = re.sub('\D', '', timestamp_text)
-    datalogger_text = re.sub('\D', '', datalogger_text)
-    if len(timestamp_text) != 9:
-        print("timestamp_text wrong", n)
-        cv2.imshow("faulty timestamp", timestamp_img)
-        cv2.waitKey(0)
-    if len(datalogger_text) != 9:
-        print("datalogger_text wrong", n)
-        cv2.imshow("faulty datalogger", datalogger_img)
-        cv2.waitKey(0)
+            timestamp_text = re.sub('\D', '', timestamp_text)
+            datalogger_text = re.sub('\D', '', datalogger_text)
 
-    l.append("timestamp_text: {timestamp_text}, datalogger_text: {datalogger_text}")
-    print(f"timestamp_text: {timestamp_text}, datalogger_text: {datalogger_text}")
-print(len(l))
+            timestamps[n] = self.__strip_unwanted_chars_validate_format(timestamp_text, n)
+            dataloggers[n] = self.__strip_unwanted_chars_validate_format(datalogger_text, n)
 
-
+            t.set_description(f"Frame {n}. Timestamp: {timestamps[n]}, Datalogger: {dataloggers[n]}")
+        return timestamps, dataloggers
